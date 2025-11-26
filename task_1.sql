4c38845ba2eb7ef41bdaca4e54dbc431b33513c1
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-11-26 19:53:10 +0530

    2 done

--- task_1.sql
+++ task_1.sql
@@ -1,3 +1,7 @@
 -- Show records of 'male' patient from 'southwest' region.
 select * from ins where gender = 'male' and region = 'southwest';
 
+
+-- Show all records having bmi in range 30 to 45 both inclusive.
+select *
+from ins where bmi between 30 and 45;
