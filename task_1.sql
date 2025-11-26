236a3a17744e9074d49770f5e0fb8a66e426c816
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-11-26 19:55:50 +0530

    3 done

--- task_1.sql
+++ task_1.sql
@@ -5,3 +5,7 @@ select * from ins where gender = 'male' and region = 'southwest';
 -- Show all records having bmi in range 30 to 45 both inclusive.
 select *
 from ins where bmi between 30 and 45;
+
+
+-- Show minimum and maximum bloodpressure of diabetic patient who smokes. Make column names as MinBP and MaxBP respectively.
+select min(ins.bloodpressure) as MinBp , max(ins.bloodpressure) as MaxBp from ins where diabetic = 'Yes' and smoker = 'Yes'
